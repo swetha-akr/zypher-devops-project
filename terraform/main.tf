be9271e8f8d0b8d6a4d8b701540ec11f62035374
swetha-akr <swethaakr11@gmail.com>
2026-03-20 23:54:13 +0530

    Update key_name for AWS instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "devops_server" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
 
-  key_name = "ubuntukey"
+  key_name = "ubuntu-key"
 
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
