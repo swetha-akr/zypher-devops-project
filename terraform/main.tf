b026085ffabd8b8349d3b69eebf235e81d948112
swetha-akr <swethaakr11@gmail.com>
2026-03-20 23:51:34 +0530

    Change key_name for AWS instance to 'ubuntukey'

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "devops_server" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
 
-  key_name = "devops-key"
+  key_name = "ubuntukey"
 
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
